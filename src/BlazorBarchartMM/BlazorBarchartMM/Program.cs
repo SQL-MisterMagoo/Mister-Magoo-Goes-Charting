85cb23484c36cc7edf4d21594cd01fd5eba78eb0
Mister Magoo <mister.magoo+githubsql@gmail.com>
2021-02-17 12:14:23 +0000

    .NET5 update

--- src/BlazorBarchartMM/BlazorBarchartMM/Program.cs
+++ src/BlazorBarchartMM/BlazorBarchartMM/Program.cs
@@ -1,16 +1,25 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BlazorBarchartMM
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+		public static async Task Main(string[] args)
+		{
+            //await Task.Delay(3000);
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+			builder.Logging.SetMinimumLevel(LogLevel.Debug);
+			builder.Services.AddLogging();
+			await builder.Build().RunAsync();
+		}
     }
 }
